b423bc9eef2a1031cce6d5c3965a8b38c86acd0e
phaccies <veronicavargas1742@gmail.com>
2024-11-25 11:57:21 -0500

    - Main Menu Level 2 Load

--- Assets/Scripts/mainMenu.cs
+++ Assets/Scripts/mainMenu.cs
@@ -9,7 +9,7 @@ public class mainMenu : MonoBehaviour
     public void Play()
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-        SceneManager.LoadScene("Tilemap Scene");
+        SceneManager.LoadScene("Level 2");
     }
 
     public void Quit()
