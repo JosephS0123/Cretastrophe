da748516e95747f4274f0101d5731c04c7d9ce20
Your Name <you@example.com>
2024-12-02 02:30:51 -0500

    made enemies shoot at non variable fire rate

--- Assets/Scripts/DrawErase/Line.cs
+++ Assets/Scripts/DrawErase/Line.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using Pathfinding;
 using Unity.VisualScripting;
 using UnityEngine;
-using static Unity.VisualScripting.Metadata;
 
 public class Line : MonoBehaviour
 {
--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -270,7 +270,6 @@ public class Projectile : MonoBehaviour
                     killingPlayer = true;
                 } else {
                     killPlayer();
-                    Destroy(gameObject);
                 }
             }
             else
